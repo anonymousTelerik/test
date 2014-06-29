733f751cd8c84f5fab209a28adffa054400335b2
drumev <milan_drumev@yahoo.com>
2014-06-29 23:27:55 +0300

    sdvsdv

---
+++ HW-Tools/LogTest AND T4 TEMPLATE/LogTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using log4net;
+using log4net.Config;
+
+public class LogTest
+{
+    private static readonly ILog logger =
+           LogManager.GetLogger(typeof(LogTest));
+
+ 
+    
+
+    static void Main(string[] args)
+    {
+        BasicConfigurator.Configure();
+        logger.Debug("LOG4NET Example:");
+        logger.Info("This is an Info log.");
+        logger.Warn("This is a warning.");
+        logger.Error("This is an error.");
+        logger.Fatal("This is a fatal error.");
+    }
+}
